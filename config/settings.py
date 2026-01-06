c8643ae7fc09a80a178ba2513c60ec4792fe2560
sarvjeet24kumar <sarvjeet@gkmit.co>
2026-01-06 18:53:10 +0530

    chore: add database migrations and drf settings

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "EXCEPTION_HANDLER": "common.exceptions.custom_exception_handler"
 }
 
 
