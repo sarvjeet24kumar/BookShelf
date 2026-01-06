07a9375c0ead940deffc28e67af3dec60c4e986a
sarvjeet24kumar <sarvjeet@gkmit.co>
2026-01-06 18:21:00 +0530

    feat: Add my-books endpoints

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BookListCreateView
+from .views import BookListCreateView, MyBookView, MyBookDetailView, BookDetailView
 
 urlpatterns = [
     path("books/", BookListCreateView.as_view(), name="list"),
-    # path("books/<uuid:id>/", AddBookView.as_view(), name="list"),
+    path("books/<uuid:id>/", BookDetailView.as_view(), name="book-detail"),
+    path("my-books/", MyBookView.as_view(), name="my-books"),
+    path("my-books/<uuid:id>/", MyBookDetailView.as_view(), name="my-books-deatails"),
 ]
