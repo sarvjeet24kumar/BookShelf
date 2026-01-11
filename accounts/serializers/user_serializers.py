b05659ad587131e0ed04a69cc8ef413340068e22
sarvjeet24kumar <sarvjeet@gkmit.co>
2026-01-11 11:44:16 +0530

    refactor: improve serializers with password validation and role support

--- accounts/serializers/user_serializers.py
+++ accounts/serializers/user_serializers.py
@@ -1,21 +1,42 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from common.validators import validate_password
 
 User = get_user_model()
 
 
-class UserListSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
             "id",
             "username",
             "email",
+            "password",
+            "phone_no",
+            "role",
             "first_name",
             "last_name",
             "created_at",
         ]
-        read_only_fields = ["id", "created_at"]
+        read_only_fields = ["id" "created_at"]
+        extra_kwargs = {
+            "email": {"required": True},
+            "username": {"required": True},
+            "phone_no": {"required": True},
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+            "password": {"write_only": True},
+            "role": {"required": False},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        password = validate_password(password)
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
@@ -26,11 +47,29 @@ class UserDetailSerializer(serializers.ModelSerializer):
             "id",
             "username",
             "email",
+            "password",
+            "phone_no",
+            "role",
             "first_name",
             "last_name",
-            "creted_at",
+            "created_at",
         ]
-        read_only_fields = fields
+        read_only_fields = ["id", "role", "created_at"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
 
 
 class SelfUserSerializer(serializers.ModelSerializer):
@@ -39,9 +78,27 @@ class SelfUserSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "username",
-            "email",
+            "username",
+            "password",
+            "phone_no",
+            "role",
             "first_name",
             "last_name",
             "created_at",
         )
-        read_only_fields = ("id", "username", "email", "created_at")
+        read_only_fields = ("id", "username", "email", "role", "created_at")
+        extra_kwargs = {
+            "password": {"write_only": True, "required": False},
+        }
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
