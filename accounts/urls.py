29567119a400694d188b74b385cc7e3296219191
sarvjeet24kumar <sarvjeet@gkmit.co>
2026-01-26 00:33:01 +0530

    feat: serializers and api endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,33 @@
 from django.urls import path
-from accounts.views.auth_views import SignupView, LoginView, LogoutView
-
-from accounts.views.user_views import UserView, UserDetailView, MeAPIView
-
+from accounts.views import (
+    SignupView,
+    VerifyEmailView,
+    ResendOTPView,
+    LoginView,
+    VerifyLoginView,
+    ResendLoginOTPView,
+    LogoutView,
+    ForgotPasswordView,
+    ResetPasswordView,
+    UserView,
+    UserDetailView,
+)
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("auth/signup/", SignupView.as_view(), name="signup"),
+    path("auth/verify-email/", VerifyEmailView.as_view(), name="verify-email"),
+    path("auth/resend-otp/", ResendOTPView.as_view(), name="resend-otp"),
+    
     path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/verify-login/", VerifyLoginView.as_view(), name="verify-login"),
+    path("auth/resend-login-otp/", ResendLoginOTPView.as_view(), name="resend-login-otp"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    
+    path("auth/forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
+    path("auth/reset-password/", ResetPasswordView.as_view(), name="reset-password"),
+    
     path("users/", UserView.as_view(), name="users"),
-    path("users/<uuid:id>/", UserDetailView.as_view(), name="user-delete"),
-    path("users/me/", MeAPIView.as_view(), name="users-me"),
+    path("users/<uuid:id>/", UserDetailView.as_view(), name="user-detail"),
 ]
